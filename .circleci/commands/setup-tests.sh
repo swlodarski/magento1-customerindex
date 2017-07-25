febab21bc521819e5fa121c4df80bad5f3c54b36
Szymon Wlodarski <szymon.wlodarski@polcode.com>
2017-07-25 11:57:36 +0200

    Issue #4 - Integrate CirceCI

---
+++ .circleci/commands/setup-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+repo=~/magento
+
+echo 'Setting up composer'
+cd "$repo"/build
+cp "$repo/.circleci/composer.json" .
+composer --global --no-interaction config repositories.this path "$repo"
+# Firegento has to come last
+composer --global --no-interaction config repositories.firegento composer https://packages.firegento.com
+composer --no-interaction --prefer-source update
+
+echo 'Removing base url from database'
+mysql --user magento --password magento -e 'DELETE FROM core_config_data WHERE path LIKE "web/%secure/base_url"\g' magento_test
+
+echo 'Configuring Magento for testing'
+cp "$repo"/.circleci/local.xml.phpunit app/etc/
+cp "$repo"/.circleci/phpunit.xml.dist .
+
+echo 'Copying module files'
+cp -R "$repo"/app/code/community/SUMOHeavy app/code/community/
+cp "$repo"/app/etc/modules/SUMOHeavy_CustomerGrid.xml app/etc/modules/
